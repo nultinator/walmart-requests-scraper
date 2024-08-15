05ef2c69b4435d37315afdd484e94b46bbf08f7d
Jake Nulty <nultinator@yahoo.com>
2024-08-15 10:56:22 -0400

    Add files via upload

---
+++ crawler-pagination.py
@@ -0,0 +1,99 @@
+import os
+import csv
+import requests
+import json
+import logging
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+
+import concurrent.futures
+from dataclasses import dataclass, field, fields, asdict
+
+API_KEY = ""
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    API_KEY = config["api_key"]
+
+
+## Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def scrape_search_results(keyword, location, page_number, retries=3):
+    formatted_keyword = keyword.replace(" ", "+")
+    url = f"https://www.walmart.com/search?q={formatted_keyword}&page={page_number+1}"
+    tries = 0
+    success = False
+    
+    while tries <= retries and not success:
+        try:
+            response = requests.get(url)
+            logger.info(f"Recieved [{response.status_code}] from: {url}")
+            if response.status_code != 200:
+                raise Exception(f"Failed to get page {page_number}, status code {response.status_code}")
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            script_tag = soup.select_one("script[id='__NEXT_DATA__'][type='application/json']")
+            json_data = json.loads(script_tag.text)
+            item_list = json_data["props"]["pageProps"]["initialData"]["searchResult"]["itemStacks"][0]["items"]
+
+            for item in item_list:
+                if item["__typename"] != "Product":
+                    continue
+                
+                name = item.get("name")
+                product_id = item["usItemId"]
+                if not name:
+                    continue
+                link = f"https://www.walmart.com/reviews/product/{product_id}"
+                price = item["price"]
+                sponsored = item["isSponsoredFlag"]
+                rating = item["averageRating"]
+                
+                search_data = {
+                    "name": name,
+                    "stars": rating,
+                    "url": link,
+                    "sponsored": sponsored,
+                    "price": price,
+                    "product_id": product_id
+                }            
+                print(search_data)                
+                
+            logger.info(f"Successfully parsed data from: {url}")
+            success = True        
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while processing page {url}: {e}")
+            logger.info(f"Retrying request for page: {url}, retries left {retries-tries}")
+            tries += 1
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+
+
+def start_scrape(keyword, pages, location, retries=3):
+    for page in range(pages):
+        scrape_search_results(keyword, location, page, retries=retries)
+
+if __name__ == "__main__":
+
+    MAX_RETRIES = 3
+    MAX_THREADS = 3
+    PAGES = 1
+    LOCATION = "us"
+
+    logger.info(f"Crawl starting...")
+
+    ## INPUT ---> List of keywords to scrape
+    keyword_list = ["laptop"]
+    aggregate_files = []
+
+    ## Job Processes
+    for keyword in keyword_list:
+        filename = keyword.replace(" ", "-")
+
+        start_scrape(keyword, PAGES, LOCATION, retries=MAX_RETRIES)
+        
+    logger.info(f"Crawl complete.")
---
+++ scraper-proxy.py
@@ -0,0 +1,287 @@
+import os
+import csv
+import requests
+import json
+import logging
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+
+import concurrent.futures
+from dataclasses import dataclass, field, fields, asdict
+
+API_KEY = ""
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    API_KEY = config["api_key"]
+
+def get_scrapeops_url(url, location="us"):
+    payload = {
+        "api_key": API_KEY,
+        "url": url,
+        "country": location,
+        }
+    proxy_url = "https://proxy.scrapeops.io/v1/?" + urlencode(payload)
+    return proxy_url
+
+## Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+@dataclass
+class SearchData:
+    name: str = ""
+    stars: float = 0
+    url: str = ""
+    sponsored: bool = False
+    price: float = 0.0
+    product_id: int = 0
+
+    def __post_init__(self):
+        self.check_string_fields()
+        
+    def check_string_fields(self):
+        for field in fields(self):
+            # Check string fields
+            if isinstance(getattr(self, field.name), str):
+                # If empty set default text
+                if getattr(self, field.name) == "":
+                    setattr(self, field.name, f"No {field.name}")
+                    continue
+                # Strip any trailing spaces, etc.
+                value = getattr(self, field.name)
+                setattr(self, field.name, value.strip())
+
+@dataclass
+class ReviewData:
+    name: str = ""
+    author_id: str = ""
+    rating: int = 0
+    date: str = ""
+    review: str = ""
+
+
+    def __post_init__(self):
+        self.check_string_fields()
+        
+    def check_string_fields(self):
+        for field in fields(self):
+            # Check string fields
+            if isinstance(getattr(self, field.name), str):
+                # If empty set default text
+                if getattr(self, field.name) == "":
+                    setattr(self, field.name, f"No {field.name}")
+                    continue
+                # Strip any trailing spaces, etc.
+                value = getattr(self, field.name)
+                setattr(self, field.name, value.strip())
+
+
+class DataPipeline:
+    
+    def __init__(self, csv_filename="", storage_queue_limit=50):
+        self.names_seen = []
+        self.storage_queue = []
+        self.storage_queue_limit = storage_queue_limit
+        self.csv_filename = csv_filename
+        self.csv_file_open = False
+    
+    def save_to_csv(self):
+        self.csv_file_open = True
+        data_to_save = []
+        data_to_save.extend(self.storage_queue)
+        self.storage_queue.clear()
+        if not data_to_save:
+            return
+
+        keys = [field.name for field in fields(data_to_save[0])]
+        file_exists = os.path.isfile(self.csv_filename) and os.path.getsize(self.csv_filename) > 0
+        with open(self.csv_filename, mode="a", newline="", encoding="utf-8") as output_file:
+            writer = csv.DictWriter(output_file, fieldnames=keys)
+
+            if not file_exists:
+                writer.writeheader()
+
+            for item in data_to_save:
+                writer.writerow(asdict(item))
+
+        self.csv_file_open = False
+                    
+    def is_duplicate(self, input_data):
+        if input_data.name in self.names_seen:
+            logger.warning(f"Duplicate item found: {input_data.name}. Item dropped.")
+            return True
+        self.names_seen.append(input_data.name)
+        return False
+            
+    def add_data(self, scraped_data):
+        if self.is_duplicate(scraped_data) == False:
+            self.storage_queue.append(scraped_data)
+            if len(self.storage_queue) >= self.storage_queue_limit and self.csv_file_open == False:
+                self.save_to_csv()
+                       
+    def close_pipeline(self):
+        if self.csv_file_open:
+            time.sleep(3)
+        if len(self.storage_queue) > 0:
+            self.save_to_csv()
+
+
+
+def scrape_search_results(keyword, location, page_number, data_pipeline=None, retries=3):
+    formatted_keyword = keyword.replace(" ", "+")
+    url = f"https://www.walmart.com/search?q={formatted_keyword}&page={page_number+1}"
+    tries = 0
+    success = False
+    
+    while tries <= retries and not success:
+        try:
+            scrapeops_proxy_url = get_scrapeops_url(url, location=location)
+            response = requests.get(scrapeops_proxy_url)
+            logger.info(f"Recieved [{response.status_code}] from: {url}")
+            if response.status_code != 200:
+                raise Exception(f"Failed to get page {page_number}, status code {response.status_code}")
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            script_tag = soup.select_one("script[id='__NEXT_DATA__'][type='application/json']")
+            json_data = json.loads(script_tag.text)
+            item_list = json_data["props"]["pageProps"]["initialData"]["searchResult"]["itemStacks"][0]["items"]
+
+            for item in item_list:
+                if item["__typename"] != "Product":
+                    continue
+                
+                name = item.get("name")
+                product_id = item["usItemId"]
+                if not name:
+                    continue
+                link = f"https://www.walmart.com/reviews/product/{product_id}"
+                price = item["price"]
+                sponsored = item["isSponsoredFlag"]
+                rating = item["averageRating"]
+                
+                search_data = SearchData(
+                    name=name,
+                    stars=rating,
+                    url=link,
+                    sponsored=sponsored,
+                    price=price,
+                    product_id=product_id
+                )                
+                data_pipeline.add_data(search_data)                
+                
+            logger.info(f"Successfully parsed data from: {url}")
+            success = True        
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while processing page {url}: {e}")
+            logger.info(f"Retrying request for page: {url}, retries left {retries-tries}")
+            tries += 1
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+
+
+def start_scrape(keyword, pages, location, data_pipeline=None, max_threads=5, retries=3):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        executor.map(
+            scrape_search_results,
+            [keyword] * pages,
+            [location] * pages,
+            range(pages),
+            [data_pipeline] * pages,
+            [retries] * pages
+        )
+
+
+def process_item(row, location, retries=3):
+    url = row["url"]
+    tries = 0
+    success = False
+
+    while tries <= retries and not success:
+        response = requests.get(get_scrapeops_url(url, location=location))
+        try:
+            if response.status_code == 200:
+                logger.info(f"Status: {response.status_code}")
+
+                soup = BeautifulSoup(response.text, "html.parser")
+                review_pipeline = DataPipeline(csv_filename=f"{row['name'].replace(' ', '-').replace('/', '')}.csv")
+                script_tag = soup.select_one("script[id='__NEXT_DATA__'][type='application/json']")
+                json_data = json.loads(script_tag.text)
+                review_list = json_data["props"]["pageProps"]["initialData"]["data"]["reviews"]["customerReviews"]
+
+                for review in review_list:
+                    name = review["userNickname"]
+                    author_id = review["authorId"]
+                    rating = review["rating"]
+                    date = review["reviewSubmissionTime"]
+                    review = review["reviewText"]
+
+                    review_data = ReviewData(
+                        name=name,
+                        author_id=author_id,
+                        rating=rating,
+                        date=date,
+                        review=review
+                    )                
+                    review_pipeline.add_data(review_data)
+
+                review_pipeline.close_pipeline()
+                success = True
+
+            else:
+                logger.warning(f"Failed Response: {response.status_code}")
+                raise Exception(f"Failed Request, status code: {response.status_code}")
+        except Exception as e:
+            logger.error(f"Exception thrown: {e}")
+            logger.warning(f"Failed to process page: {row['url']}")
+            logger.warning(f"Retries left: {retries-tries}")
+            tries += 1
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+    else:
+        logger.info(f"Successfully parsed: {row['url']}")
+        
+
+def process_results(csv_file, location, max_threads=5, retries=3):
+    logger.info(f"processing {csv_file}")
+    with open(csv_file, newline="") as file:
+        reader = list(csv.DictReader(file))
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+            executor.map(
+                process_item,
+                reader,
+                [location] * len(reader),
+                [retries] * len(reader)
+            )
+
+if __name__ == "__main__":
+
+    MAX_RETRIES = 3
+    MAX_THREADS = 3
+    PAGES = 4
+    LOCATION = "us"
+
+    logger.info(f"Crawl starting...")
+
+    ## INPUT ---> List of keywords to scrape
+    keyword_list = ["laptop"]
+    aggregate_files = []
+
+    ## Job Processes
+    for keyword in keyword_list:
+        filename = keyword.replace(" ", "-")
+
+        crawl_pipeline = DataPipeline(csv_filename=f"{filename}.csv")
+        start_scrape(keyword, PAGES, LOCATION, data_pipeline=crawl_pipeline, max_threads=MAX_THREADS, retries=MAX_RETRIES)
+        crawl_pipeline.close_pipeline()
+        aggregate_files.append(f"{filename}.csv")
+    logger.info(f"Crawl complete.")
+
+    for file in aggregate_files:
+        process_results(file, LOCATION, max_threads=MAX_THREADS, retries=MAX_RETRIES)
+
+    
